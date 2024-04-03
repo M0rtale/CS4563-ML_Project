2c85e9f7013e47c342bf24b76905beb3d7b32b19
dvv112345 <dvv112345@gmail.com>
2024-04-03 17:46:26 -0400

    Global optimization

--- src/ml.py
+++ src/ml.py
@@ -3,11 +3,11 @@ import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
 import argparse
-
+import time
 
 TARGET = "MM256"
 DEBUG = True
-USE_PRUNE = True
+USE_PRUNE = False
 DEVICE = 'cpu'
 NUM_ITER = 10000
 LEARNING_RATE = 0.001
@@ -35,63 +35,47 @@ def MSE(predicted:torch.tensor, actual:torch.tensor) -> torch.tensor:
     loss = torch.sum(diff_squared) / predicted.shape[0]
     return loss
 
-def train(data: torch.tensor, targetIndex: int, num_iter:int, lr:float) -> torch.tensor:
+def train(data: torch.tensor, targetIndex: int) -> torch.tensor:
     '''Kickstarts the traninig process of the dataset, assumes the data is normalized'''
     y = data[:, targetIndex].reshape((data.shape[0], 1))
     X_first = data[:,:targetIndex]
     X_second = data[:, targetIndex+1:]
     X = torch.hstack([X_first, X_second])
-    w = torch.zeros((X.shape[1], 1), dtype = torch.float64, requires_grad=True).to(DEVICE)
+    start = time.time()
     w_global = torch.linalg.pinv(X).matmul(y)
+    end = time.time()
+    LOG("Time for global optimization:", end-start)
     
     LOG("y shape:", y.shape)
-    LOG("w shape:", w.shape)
     LOG("X shape:", X.shape)
-    optimizer = torch.optim.SGD([w], lr=lr)
-
-    writer = SummaryWriter(log_dir=f"../log/{EXP_NAME}")
-
-
-    for i in range(num_iter):
-        pred = torch.matmul(X, w)
-        loss = MSE(pred, y)
-        writer.add_scalar("Loss", loss, global_step=i)
-        loss.backward()
-        optimizer.step()
-        #LOG(w)
     
-    LOG(w_global)
     pred = torch.matmul(X, w_global)
     loss = MSE(pred, y)
     LOG("Global loss:", loss)
-    pred = torch.matmul(X, w)
-    loss = MSE(pred, y)
-    LOG("Local loss:", loss)
-    return w
+    return w_global
 
 def main() -> None:
     '''this is the entry of the program.
     {r}'''
+    start = time.time()
     dataset, meta = get_data()
+    end = time.time()
+    LOG("Time for global optimization:", end-start)
     data = np.array(dataset.tolist(), dtype=np.float64)
     data = torch.from_numpy(data).to(DEVICE)
     data = torch.nn.functional.normalize(data)
     LOG("Data shape:", data.shape)
 
     #send to train
-    w = train(data, meta.names().index(TARGET), NUM_ITER, LEARNING_RATE)
+    w = train(data, meta.names().index(TARGET))
     LOG(w)
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser() 
     parser.add_argument("--exp", default="test", type=str) 
-    parser.add_argument("--lr", default=0.01, type=float) 
-    parser.add_argument("--iter", default=1000, type=int) 
     args = parser.parse_args()
     EXP_NAME = args.exp
-    LEARNING_RATE = args.lr
-    NUM_ITER = args.iter
     if torch.cuda.is_available():
         LOG("Cuda is available, switching to cuda")
         DEVICE = "cuda"
