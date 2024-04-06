511491c616bd209342ae3f6b2085e783ef3a4e4f
Allen <pro.juda@gmail.com>
2024-04-06 16:31:19 -0400

    first implementation

--- src/dataset_warmup.py
+++ src/dataset_warmup.py
@@ -4,23 +4,23 @@ import ray
 from scipy.io import arff
 from time import sleep
 
-@ray.remote
-class DataStreamer:
-    def __init__(self, file_path):
-        self.dataset, self.meta = arff.loadarff(file_path)
+# @ray.remote
+# class DataStreamer:
+#     def __init__(self, file_path):
+#         self.dataset, self.meta = arff.loadarff(file_path)
 
-    def get_data(self):
-        return self.dataset, self.meta
+#     def get_data(self):
+#         return self.dataset, self.meta
 
 if __name__ == "__main__":
     ray.init()  # Initialize Ray
 
-    # Assuming the dataset file is in the correct location
-    file_path = '/home/allen/CS4563-ML_Project/dataset/pruned.arff'
-    data_streamer = DataStreamer.remote(file_path)
+    dataset, meta = arff.loadarff("../dataset/pruned.arff")
 
-    # Optionally, register the actor under a global name for easy access across processes
-    #ray.register_actor("global_data_streamer", data_streamer)
+    ray.put(dataset)
+
+    ray.put(meta)
+
     print("DataStreamer is ready and serving data.")
 
     while True:
